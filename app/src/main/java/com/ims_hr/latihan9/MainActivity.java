89d970ab3f917a62a96a1de9fc933ce439715a98
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-10-28 09:28:02 +0700

    UPDATE 20211028

--- app/src/main/java/com/ims_hr/latihan9/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan9/MainActivity.java
@@ -1,10 +1,8 @@
 package com.ims_hr.latihan9;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.Toast;
@@ -12,8 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     RatingBar RB_Rate;
-    Button B_Submit;
-    Button B_Exit;
+    Button B_Submit, B_Exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,53 +28,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void Listen_B_Submit() {
-        B_Submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                float Rating = RB_Rate.getRating();
-                String Kategori = "";
-                if(Rating >= 5) {
-                    Kategori = "Sangat Setuju";
-                } else if(Rating >= 4) {
-                    Kategori = "Setuju";
-                } else if(Rating >= 3) {
-                    Kategori = "Kurang Setuju";
-                } else if(Rating >= 2) {
-                    Kategori = "Tidak Setuju";
-                } else {
-                    Kategori = "Sangat Tidak Setuju";
-                }
-                String Pesan = "Nilai rating adalah " + Rating +
-                        " yang artinya saya " + Kategori;
-                Toast.makeText(MainActivity.this, Pesan, Toast.LENGTH_LONG).show();
+        B_Submit.setOnClickListener(v -> {
+            float Rating = RB_Rate.getRating();
+            String Kategori = "";
+            if(Rating >= 5) {
+                Kategori = "Sangat Setuju";
+            } else if(Rating >= 4) {
+                Kategori = "Setuju";
+            } else if(Rating >= 3) {
+                Kategori = "Kurang Setuju";
+            } else if(Rating >= 2) {
+                Kategori = "Tidak Setuju";
+            } else {
+                Kategori = "Sangat Tidak Setuju";
             }
+            String Pesan = "Nilai rating adalah " + Rating +
+                    " yang artinya saya " + Kategori;
+            Toast.makeText(MainActivity.this, Pesan, Toast.LENGTH_LONG).show();
         });
     }
 
     private void Listen_B_Exit() {
-        B_Exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String Konfirmasi = getResources().getString(R.string.MainActivity_Konfirmasi);
-                AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
-                a_builder.setMessage(Konfirmasi)
-                        .setCancelable(false)
-                        .setPositiveButton("Iya", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                finish();
-                            }
-                        })
-                        .setNegativeButton("Nggak", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                            }
-                        });
-                AlertDialog alert = a_builder.create();
-                alert.setTitle("Konfirmasi");
-                alert.show();
-            }
+        B_Exit.setOnClickListener(v -> {
+            String Konfirmasi = getResources().getString(R.string.MainActivity_Konfirmasi);
+            AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+            /*
+            a_builder.setMessage(Konfirmasi)
+                    .setCancelable(false)
+                    .setPositiveButton("Iya", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("Nggak", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+            */
+            a_builder.setMessage(Konfirmasi)
+                    .setCancelable(false)
+                    .setPositiveButton("Iya", (dialog, which) -> finish())
+                    .setNegativeButton("Nggak", (dialog, which) -> dialog.cancel());
+
+            AlertDialog alert = a_builder.create();
+            alert.setTitle("Konfirmasi");
+            alert.show();
         });
     }
 
